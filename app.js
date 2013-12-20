f9728bee4057baa88ccf58fb1b3ce55f4f473c70
michael <michael.ivankiv@effectiveui.com>
2013-12-21 00:33:27 +0200

    Updated design of search.

--- app.js
+++ app.js
@@ -1,90 +1,80 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express(),
+    Q = require("q"),
+    qFS = require("q-io/fs"),
+    handlebars = require("handlebars"),
 
-var Q = require("q");
-var qFS = require("q-io/fs");
-var handlebars = require("handlebars")
-
-
-var mongojs = require('mongojs');
-var db = mongojs("poetry");
-var collection = db.collection('poetry');
+    mongojs = require('mongojs'),
+    db = mongojs("poetry"),
+    collection = db.collection('poetry');
 
 function randomFromInterval(from,to){
     return Math.floor(Math.random()*(to-from+1)+from);
 }
 
+app
+    .use(express.bodyParser())
+    .use('/assets', express.static(__dirname + '/assets'))
+    .use('/bower_components',express.static(__dirname + '/bower_components'))
 
-app.use(express.bodyParser());
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/bower_components',express.static(__dirname + '/bower_components'));
+    .get('/', function(req, res){
+        qFS.read('assets/index.html')
+            .then(function(template){
+                return handlebars.compile(template);
+            })
+            .then (function(template){
+                var deferred = Q.defer();
 
-app.get('/', function(req, res){
-    qFS.read('assets/index.html')
-        .then(function(template){
-            return handlebars.compile(template);
-        })
-        .then (function(template){
-            var deferred = Q.defer();
+                collection.find().count(function(err, count){
+                    var skip = Math.round(randomFromInterval(0, count - 1));
 
-            collection.find().count(function(err, count){
-                var skip = Math.round(randomFromInterval(0, count - 1));
-
-                collection.find({}).skip(skip).limit(1).toArray(function(err, items) {
-                    deferred.resolve (template(items[0]))
+                    collection.find({}).skip(skip).limit(1).toArray(function(err, items) {
+                        deferred.resolve (template(items[0]))
+                    });
                 });
-            });
 
-            return deferred.promise;
-        })
-        .then(function(html){
-            res.send(html);
-        })
-        .fail(function(err){
-            console.log(err);
-        })
-});
+                return deferred.promise;
+            })
+            .then(function(html){
+                res.send(html);
+            })
+            .fail(function(err){
+                console.log(err);
+            })
+    })
 
-app.get('/search', function(req, res){
-    collection.find({
-        $or: [
-            { author : { $regex: req.query.query, $options: 'i' }},
-            { poem   : { $regex: req.query.query, $options: 'i' }}
-        ]})
-        .limit(10)
-        .toArray(function(err, data){
-            res.send({items: data});
-        })
-
-    /**
-     * Temporarily disabled full text search.
-     */
-//    collection.runCommand("text", {search:req.query.query, language: "russian"}, function(err, data) {
-//        res.send({items: data.results.map(function(el){return el.obj})});
-//    });
-})
+    .get('/search', function(req, res){
+        collection.find({
+            $or: [
+                { author : { $regex: req.query.query, $options: 'i' }},
+                { poem   : { $regex: req.query.query, $options: 'i' }}
+            ]})
+            .limit(10)
+            .toArray(function(err, data){
+                res.send({items: data});
+            })
+    })
 
 
+    .post('/add', function(req, res){
+        collection.insert(req.body, {w:1}, function(err, result) {
+            res.send("Ok")
+        });
+    })
 
-app.post('/add', function(req, res){
-    collection.insert(req.body, {w:1}, function(err, result) {});
-    res.send("Ok")
-});
-
-app.get('/new', function(req, res){
-    qFS.read('assets/add.html')
-        .then(function(template){
-            return handlebars.compile(template);
-        })
-        .then(function(template){
-            return template({});
-        })
-        .then(function(html){
-            res.send(html);
-        })
-        .fail(function(err){
-            console.log(err);
-        })
-});
-
-app.listen(3000);
+    .get('/new', function(req, res){
+        qFS.read('assets/add.html')
+            .then(function(template){
+                return handlebars.compile(template);
+            })
+            .then(function(template){
+                return template({});
+            })
+            .then(function(html){
+                res.send(html);
+            })
+            .fail(function(err){
+                console.log(err);
+            })
+    })
+    .listen(3000);
