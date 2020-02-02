670cb45bbadcec7c9d764bf912534154a03ca097
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2020-02-02 21:47:25 +0100

    Setup graph-ql

---
+++ graph-ql-api/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import graphql from "graphql";
+
+const { buildSchema } = graphql;
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => "Hello world!" };
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+app.listen(4000, () => console.log("Now browse to localhost:4000/graphql"));
